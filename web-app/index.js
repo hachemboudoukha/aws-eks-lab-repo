844dd420793cfec0d1c197f85174579bbe39e9ba
hachemboudoukha <hachemboudoukha@gmail.com>
2026-04-07 17:36:39 +0200

    cache HTML in memory for faster requests

--- web-app/index.js
+++ web-app/index.js
@@ -1,12 +1,25 @@
 import http from 'http';
 import fs from 'fs';
 
-const PORT = 8000
+const PORT = 8000;
+let cachedHTML = null;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('index.html').pipe(res)
-})
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  if (cachedHTML) {
+    res.end(cachedHTML);
+  } else {
+    fs.readFile('index.html', 'utf-8', (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('Error loading page');
+        return;
+      }
+      cachedHTML = data;
+      res.end(data);
+    });
+  }
+});
 
 console.log(`Server listening on port ${PORT}`);
-server.listen(process.env.PORT || PORT)
+server.listen(process.env.PORT || PORT);
